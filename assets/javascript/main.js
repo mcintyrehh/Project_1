91132873aacb20d7a1d55c24a1c54894be0b9a20
Onray <mcintyrehh@gmail.com>
2018-07-28 11:22:35 -0400

    testing sdk addition

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,10 @@
 var token;
-(function() {
+(function () {
   'use strict';
 
-  var ViewModel = function() {
+  var ViewModel = function () {
     this.isLoggedIn = ko.observable(false);
-    this.login = function() {
+    this.login = function () {
       var self = this;
       this.loginErrorMessage(null);
       OAuthManager.obtainToken({
@@ -14,29 +14,29 @@ var token;
             automatically when obtaining an access token through
             the user login form
             */
-            'user-read-private'
-          ]
-        }).then(function(token) {
-          onTokenReceived(token);
-          console.log(token);
-        }).catch(function(error) {
-          self.loginError(error);
-        });
+          'user-read-private'
+        ]
+      }).then(function (token) {
+        onTokenReceived(token);
+        console.log(token);
+      }).catch(function (error) {
+        self.loginError(error);
+      });
     };
 
-    this.logout = function() {
+    this.logout = function () {
       localStorage.removeItem(accessTokenKey);
       this.isLoggedIn(false);
     };
 
-    this.loginError = function(errorCode) {
+    this.loginError = function (errorCode) {
       switch (errorCode) {
         case 'access_denied':
           this.loginErrorMessage('You need to grant access in order to use this website.');
           break;
         default:
           this.loginErrorMessage('There was an error trying to obtain authorization. Please, try it again later.');
-        }
+      }
     };
 
     this.loginErrorMessage = ko.observable(null);
@@ -50,7 +50,7 @@ var token;
   ko.applyBindings(viewModel);
 
   var spotifyApi = new SpotifyWebApi(),
-      accessTokenKey = 'sp-access-token';
+    accessTokenKey = 'sp-access-token';
 
   function onTokenReceived(token) {
     viewModel.isLoggedIn(true);
@@ -58,43 +58,17 @@ var token;
 
     // do something with the token
     spotifyApi.setAccessToken(token);
-    spotifyApi.getMe().then(function(data) {
+    spotifyApi.getMe().then(function (data) {
       viewModel.user(data);
 
-      spotifyApi.getUserPlaylists(data.id).then(function(playlists) {
+      spotifyApi.getUserPlaylists(data.id).then(function (playlists) {
         console.log(playlists);
         viewModel.playlists(playlists.items);
       });
     });
+    token = token;
   }
-  window.onSpotifyWebPlaybackSDKReady = () => {
-  const player = new Spotify.Player({
-    name: 'PicMeUp',
-    getOAuthToken: cb => { cb(token); }
-  });
-
-  // Error handling
-  player.addListener('initialization_error', ({ message }) => { console.error(message); });
-  player.addListener('authentication_error', ({ message }) => { console.error(message); });
-  player.addListener('account_error', ({ message }) => { console.error(message); });
-  player.addListener('playback_error', ({ message }) => { console.error(message); });
-
-  // Playback status updates
-  player.addListener('player_state_changed', state => { console.log(state); });
-
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id);
-  });
-
-  // Not Ready
-  player.addListener('not_ready', ({ device_id }) => {
-    console.log('Device ID has gone offline', device_id);
-  });
-
-  // Connect to the player!
-  player.connect();
-}
+  
 
   /**
    * Uses the stored access token
