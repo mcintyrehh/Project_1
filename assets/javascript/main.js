99ac5e988cfc1f72dc28c4bd91e450f1f1d5bdf7
Onray <mcintyrehh@gmail.com>
2018-07-28 12:00:50 -0400

    testeroni

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -81,7 +81,7 @@
           console.log('The Web Playback SDK successfully connected to Spotify!');
         }
       })
-      playerplayer.addListeneraddListener('ready', ({ device_id }) => {
+      player.addListeneraddListener('ready', ({ device_id }) => {
         console.log('The Web Playback SDK is ready to play music!');
         console.log('Device ID', device_id);
       })
