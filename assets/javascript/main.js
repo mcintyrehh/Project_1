0d798b87096883892a8217af808824c56ee6419b
Onray <mcintyrehh@gmail.com>
2018-07-29 19:32:01 -0400

    mmmmmmmmmmmmm

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -138,9 +138,10 @@
         //   .done(function (data) {
               url: 'https://api.spotify.com/v1/me/player/play?device_id=22dc7f75b1abd6b1252720ef5c76bddbb9165ccc',
               type: 'PUT',
-              context: {
-                uri:"spotify:user:124239502:playlist:70Vhwte8On581mDvi2F98F",
-              },
+              // data: {
+              //   "context_uri": "spotify:user:124239502:playlist:70Vhwte8On581mDvi2F98F"
+              // },
+              data: {'\"context_uri\":\"spotify:user:124239502:playlist:70Vhwte8On581mDvi2F98F\"'},
               headers: {
                 'Authorization': 'Bearer ' + playToken,
               },
