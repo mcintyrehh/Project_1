497dcfd19c960852aae8171f05fa9138502ed6d8
Onray <mcintyrehh@gmail.com>
2018-07-29 18:01:49 -0400

    might be working lol

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -141,6 +141,8 @@
           $.ajax({
             url: 'https://api.spotify.com/v1/me/player/play?device_id=22dc7f75b1abd6b1252720ef5c76bddbb9165ccc',
            type: 'PUT', 
+           data: {
+             'context_uri': 'spotify:user:124239502:playlist:70Vhwte8On581mDvi2F98F'},
            headers: { 
               'Authorization': 'Bearer ' + playToken,
              
