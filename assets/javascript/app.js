e34e6706e473cc03bf921e3944ad82717a1180aa
Onray <mcintyrehh@gmail.com>
2018-07-28 21:41:56 -0400

    maybe not

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,15 @@
 
 $(document).ready(function () {
-    console.log('location: ' + location.href)
+    $('.playbtn').on('click', function () {
+        console.log('at least the click worked');
+        var userInfo = '/users/124239502'
+        var happyPlaylist = '/playlists/70Vhwte8On581mDvi2F98F'
+        var queryURL = 'https://api.spotify.com/v1' + userInfo + happyPlaylist;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+    });
 });
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -62,18 +62,18 @@
       viewModel.user(data);
       
       //playlist pull!
-     $('.playbtn').on('click', function() {
-      console.log('at least the click worked');
-      var userInfo = '/users/124239502'
-      var happyPlaylist = '/playlists/70Vhwte8On581mDvi2F98F'
-      var queryURL = 'https://api.spotify.com/v1' + userInfo + happyPlaylist;
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
-        console.log(response);
-      });
-      });
+    //  $('.playbtn').on('click', function() {
+    //   console.log('at least the click worked');
+    //   var userInfo = '/users/124239502'
+    //   var happyPlaylist = '/playlists/70Vhwte8On581mDvi2F98F'
+    //   var queryURL = 'https://api.spotify.com/v1' + userInfo + happyPlaylist;
+    //   $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   }).then(function (response) {
+    //     console.log(response);
+    //   });
+    //   });
         // spotifyApi.getUserPlaylists(data.id).then(function (playlists) {
         //   console.log(playlists);
         //   viewModel.playlists(playlists.items);
