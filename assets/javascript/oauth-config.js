39fac54ae49257563cbb0023f8fffd794f891045
Jcontratto <jmc22@hotmail.com>
2018-07-29 19:50:19 -0400

    NEWEST COMMIT

---
+++ assets/javascript/oauth-config.js
@@ -0,0 +1,22 @@
+/*exported OAuthConfig*/
+var OAuthConfig = (function() {
+    'use strict';
+  
+    /* replace these values with yours obtained in the
+    "My Applications" section of the Spotify developer site */
+    var clientId = '2e12ca59d482427694678b6f76ce6cac';
+    var redirectUri = 'https://mcintyrehh.github.io/Project_1/callback.html';
+  
+    if (location.href.indexOf('https://mcintyrehh.github.io') === 0) {
+      redirectUri = 'https://mcintyrehh.github.io/Project_1/callback.html';
+    }
+  
+    var host = /http[s]?:\/\/[^/]+/.exec(redirectUri)[0];
+  
+    return {
+      clientId: clientId,
+      redirectUri: redirectUri,
+      host: host,
+      stateKey: 'spotify_auth_state'
+    };
+  })();
