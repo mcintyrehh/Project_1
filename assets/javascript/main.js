11590f5dd7cdc12e68f898e88a453d4b8fe4f836
Onray <mcintyrehh@gmail.com>
2018-07-30 12:08:12 -0400

    moved where deviceId was set

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -104,11 +104,12 @@
       // Ready
       player.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id);
+        deviceId = device_id;
       });
       // Not Ready
       player.addListener('not_ready', ({ device_id }) => {
         console.log('Device ID has gone offline', device_id);
-        deviceId = device_id; 
+         
       });
       // Connect to the player!
       player.connect().then(success => {
